3e4427ee682de76d4912113391098957a134ff36
Baudi <baudirguanche@gmail.com>
2023-08-19 19:03:53 +0100

    Actualización de configuraciones y servicios en firebase.js.
    - Añadida la importación y inicialización de Firebase Storage.
    - Exportado storage para su uso en otros componentes.
    - Realizadas pequeñas correcciones y mejoras en las configuraciones existentes.

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
     apiKey: "AIzaSyB14zochkvee2M-NQbS5B44IHBeXlhxL7U",
@@ -14,5 +16,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
+const firestore = getFirestore(app);
+const storage = getStorage(app);
 
-export { auth, provider, signInWithPopup, signOut };
+export { auth, provider, signInWithPopup, signOut, firestore, storage };
