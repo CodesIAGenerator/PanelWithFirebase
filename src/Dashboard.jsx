cccce2dd85c30b736b167bceeac258107638b5d9
Baudi <baudirguanche@gmail.com>
2023-08-19 19:04:00 +0100

    Refactorización de Dashboard para mejorar la navegación y la interacción del usuario.
    - Añadida la lógica para mostrar el componente Perfil al hacer clic en el avatar.
    - Implementada la función renderContent para determinar el contenido a mostrar según el menú seleccionado.
    - Añadido manejo de roles para mostrar el menú de Usuarios solo a administradores.
    - Actualizado el menú del sidebar para reflejar el ítem seleccionado y corregido el comportamiento de navegación.

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,26 +1,97 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Layout, Menu, Button, Avatar } from 'antd';
-import { auth, signOut } from './firebase';
+import { auth, signOut, firestore } from './firebase'; // Asegúrate de importar firestore
+import Perfil from './Perfil';
 import './Dashboard.css';
-
+import Usuarios from './Usuarios';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { MenuItem } from '@mui/material';
 const { Header, Content, Footer, Sider } = Layout;
+
 
 function Dashboard() {
   const [userPhoto, setUserPhoto] = useState(null);
+  const [selectedMenu, setSelectedMenu] = useState('1');
+  const [userRole, setUserRole] = useState(null);
+  const [userPhotoURL, setUserPhotoURL] = useState(null);
+  const [showProfile, setShowProfile] = useState(false);
+
+  const handleAvatarClick = () => {
+    setShowProfile(true);
+    setSelectedMenu('3');
+};
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
+    const unsubscribe = auth.onAuthStateChanged(async user => {
       if (user) {
         setUserPhoto(user.photoURL);
+
+        // Obtener el rol del usuario desde Firestore
+        const userRef = doc(firestore, 'users', user.uid);
+        const docSnapshot = await getDoc(userRef);
+        if (docSnapshot.exists()) {
+          const userData = docSnapshot.data();
+          setUserRole(userData.role);  // <-- Establece el rol aquí
+        } else {
+          await setDoc(userRef, {
+            email: user.email,
+            role: 'user'
+          }, {merge: true});
+          setUserRole('user');  // <-- Establece el rol por defecto aquí
+        }
       }
     });
 
     return () => unsubscribe();
   }, []);
 
+  useEffect(() => {
+    const fetchUserData = async () => {
+        if (auth.currentUser) {
+            const userRef = doc(firestore, 'users', auth.currentUser.uid);
+            const docSnapshot = await getDoc(userRef);
+            if (docSnapshot.exists()) {
+                const data = docSnapshot.data();
+                setUserPhotoURL(data.photoURL || null);
+            }
+        }
+    };
+
+    const unsubscribe = auth.onAuthStateChanged(user => {
+        if (user) {
+            setUserPhoto(user.photoURL);
+            fetchUserData();
+        }
+    });
+
+    return () => unsubscribe();
+}, []);
+
+
+
   const handleLogout = () => {
     signOut(auth);
   };
+
+
+  const renderContent = () => {
+    if (showProfile) {
+        return <Perfil />;
+    }
+    switch (selectedMenu) {
+        case '2':
+            return <Usuarios />;
+        case '3':
+            return <Perfil />;    
+        default:
+            return <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                Contenido principal del panel
+            </div>;
+    }
+};
+
+
+  console.log(userRole);
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
@@ -29,23 +100,24 @@ function Dashboard() {
         collapsedWidth="0"
       >
         <div className="logo" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-          <Menu.Item key="1">Opción 1</Menu.Item>
-          <Menu.Item key="2">Opción 2</Menu.Item>
-          <Menu.Item key="3">Opción 3</Menu.Item>
+        <Menu theme="dark" mode="inline" selectedKeys={[selectedMenu]} onSelect={({ key }) => {
+          setSelectedMenu(key);
+          setShowProfile(false);
+        }}>
+          <Menu.Item key="1">Inicio</Menu.Item>
+          {userRole === 'admin' && <Menu.Item key="2">Usuarios</Menu.Item>}
+          <Menu.Item key="3">Perfil</Menu.Item>
         </Menu>
       </Sider>
       <Layout>
         <Header className="site-layout-sub-header-background">
-          {userPhoto && <Avatar src={userPhoto} style={{ marginRight: '10px' }} />}
+        <Avatar src={userPhotoURL || userPhoto} style={{ marginRight: '10px' }} onClick={handleAvatarClick} />
           <Button type="primary" onClick={handleLogout}>
             Cerrar sesión
           </Button>
         </Header>
         <Content style={{ margin: '24px 16px 0' }}>
-          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-            Contenido principal del panel
-          </div>
+          {renderContent()}
         </Content>
         <Footer style={{ textAlign: 'center' }}>Tu Empresa ©2023 Creado por Tu Nombre</Footer>
       </Layout>
