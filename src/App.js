0b93dc32109ca66aaff36ba0a7fb302ceb30d0df
Baudi <baudirguanche@gmail.com>
2023-08-21 09:17:24 +0100

    Se ha envuelto las rutas en <AuthProvider> para protegerla con acceso de usuario y 2fa

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Dashboard from './Dashboard';
 import ProtectedRoute from './ProtectedRoute';
+import { AuthProvider } from './auth-context'; // Asegúrate de importar AuthProvider
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<ProtectedRoute />}>
-          <Route index element={<Dashboard />} />
-        </Route>
-      </Routes>
+      <AuthProvider> {/* Envuelve tu aplicación con AuthProvider */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
