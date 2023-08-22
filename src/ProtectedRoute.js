e61abf06846f407297e7fd08fbed298f0326626b
Baudi <baudirguanche@gmail.com>
2023-08-22 17:47:06 +0100

    Refactorización y corrección de la lógica de rutas protegidas en ProtectedRoute.js, mejorando la verificación y redirección.

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth, firestore } from './firebase';
 import { doc, getDoc } from 'firebase/firestore';
-import { message } from 'antd';
 
 function ProtectedRoute({ children }) {
   const navigate = useNavigate();
@@ -11,21 +10,20 @@ function ProtectedRoute({ children }) {
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
-        console.log("UserID establecido:", user.uid);  // Verifica si se establece el userId
+        console.log("UserID establecido:", user.uid); // Verifica si se establece el userId
         setUserId(user.uid);
       }
     });
 
     return () => unsubscribe();
-}, []);
-
+  }, []);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (!user) {
         navigate('/');
       } else if (localStorage.getItem('twoFACompleted') !== 'true') {
-        navigate('/dashboard');  // Redirige a la página de verificación de dos pasos
+        navigate('/dashboard'); // Redirige a la página de verificación de dos pasos
       }
     });
 
@@ -33,15 +31,14 @@ function ProtectedRoute({ children }) {
   }, [navigate]);
 
   useEffect(() => {
-    const checkField = async(userId) => {
-
+    const checkField = async (userId) => {
       try {
         const userRef = doc(firestore, 'users', userId);
-         const docSnapshop = await getDoc(userRef);
+        const docSnapshop = await getDoc(userRef);
 
-         console.log(docSnapshop.exists());
+        console.log(docSnapshop.exists());
 
-         if (docSnapshop.exists()) {
+        if (docSnapshop.exists()) {
           const data = docSnapshop.data();
           if (!("twoFAEnabled" in data)) {
             navigate('/dashboard');
@@ -51,24 +48,17 @@ function ProtectedRoute({ children }) {
           if (!docSnapshop.data().twoFAVerified) {
             navigate('/');
           }
-         }
+        }
       } catch (error) {
         console.log(error);
       }
-         
-        
+    };
+
+    if (userId) {
+      checkField(userId);
     }
 
-    checkField(userId);
-    
-  }, [userId]);
- 
-
-  //  // Verificar si el usuario ha completado la verificación de dos pasos
-  //  if (localStorage.getItem('twoFACompleted') !== 'true') {
-  //   console.log("No se puede entrar");
-  //    return <div>Por favor, completa la verificación de dos pasos para acceder al dashboard.</div>;
-  //  }
+  }, [userId, navigate]);
 
   return children;
 }
