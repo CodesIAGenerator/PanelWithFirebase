37489d12ab8b8d5b5dd5ef1b213488d4ea594428
Baudi <baudirguanche@gmail.com>
2023-08-27 22:59:18 +0100

    Actualización del componente Dashboard para incluir funcionalidades de autenticación, manejo de roles de usuario, selección de menú y modo oscuro.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { Layout, Menu, Button, Avatar } from 'antd';
-import { auth, firestore } from '../firebase/firebase';
 import Perfil from '../components/Perfil';
-import '../Dashboard.css';
 import Usuarios from '../components/Usuarios';
+import Proyectos from '../components/Proyectos';
+import Tareas from '../components/Tareas';
+import '../Dashboard.css';
 import { doc, getDoc, setDoc, updateDoc } from 'firebase/firestore';
-const { Header, Content, Footer, Sider } = Layout;
+import { auth, firestore } from '../firebase/firebase';
+import { FloatButton } from 'antd';
+import { BulbOutlined, LogoutOutlined } from '@ant-design/icons';
+import Precios from '../components/Precios';
 
+const { Header, Content, Footer, Sider } = Layout;
 
 function Dashboard() {
   const [userPhoto, setUserPhoto] = useState(null);
@@ -14,139 +19,142 @@ function Dashboard() {
   const [userRole, setUserRole] = useState(null);
   const [userPhotoURL, setUserPhotoURL] = useState(null);
   const [showProfile, setShowProfile] = useState(false);
-  
+  const [proyectoSeleccionado, setProyectoSeleccionado] = useState(null);
+  const [darkMode, setDarkMode] = useState(false);
+
+
+  // Colores
+  const menuStyle = darkMode ? { backgroundColor: '#000', color: '#fff' } : { backgroundColor: 'inherit', color: '#000' };
+const menuItemStyle = darkMode ? { color: '#fff' } : { color: '#000' };
+const selectedMenuItemStyle = darkMode ? { color: '#000', backgroundColor: '#aaa' } : { color: '#000', backgroundColor: '#e6e6e6' };
+
+
+
+
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
 
   const handleAvatarClick = () => {
     setShowProfile(true);
     setSelectedMenu('3');
-};
-
-useEffect(() => {
-  const unsubscribe = auth.onAuthStateChanged(async user => {
-    if (user) {
-      setUserPhoto(user.photoURL);
-      const userRef = doc(firestore, 'users', user.uid);
-      const docSnapshot = await getDoc(userRef);
-      if (docSnapshot.exists()) {
-        const userData = docSnapshot.data();
-        setUserRole(userData.role);
-      } else {
-        await setDoc(userRef, {
-          email: user.email,
-          role: 'user'
-        }, {merge: true});
-        setUserRole('user');
-      }
-      const fetchUserData = async () => {
-          const userRef = doc(firestore, 'users', auth.currentUser.uid);
-          const docSnapshot = await getDoc(userRef);
-          if (docSnapshot.exists()) {
-              const data = docSnapshot.data();
-              setUserPhotoURL(data.photoURL || null);
-          }
-      };
-      fetchUserData();
-    }
-  });
-
-  return () => unsubscribe();
-}, []);
+  };
 
   useEffect(() => {
-    const fetchUserData = async () => {
-        if (auth.currentUser) {
-            const userRef = doc(firestore, 'users', auth.currentUser.uid);
-            const docSnapshot = await getDoc(userRef);
-            if (docSnapshot.exists()) {
-                const data = docSnapshot.data();
-                setUserPhotoURL(data.photoURL || null);
-            }
+    const unsubscribe = auth.onAuthStateChanged(async user => {
+      if (user) {
+        const fetchUserData = async () => {
+          const userRef = doc(firestore, 'users', user.uid);
+          const docSnapshot = await getDoc(userRef);
+          if (docSnapshot.exists()) {
+            const data = docSnapshot.data();
+            setUserPhotoURL(data.photoURL || user.photoURL);
+          }
+        };
+        fetchUserData();
+  
+        const userRef = doc(firestore, 'users', user.uid);
+        const docSnapshot = await getDoc(userRef);
+        if (docSnapshot.exists()) {
+          const userData = docSnapshot.data();
+          setUserRole(userData.role);
+        } else {
+          await setDoc(userRef, {
+            email: user.email,
+            role: 'user'
+          }, { merge: true });
+          setUserRole('user');
         }
-    };
-
-    const unsubscribe = auth.onAuthStateChanged(user => {
-        if (user) {
-            setUserPhoto(user.photoURL);
-            fetchUserData();
-        }
+      }
     });
-
+  
     return () => unsubscribe();
-}, []);
+  }, []);
 
-
-const handleLogout = async () => {
-  const userId = auth.currentUser?.uid;
-  if (userId) {
-    await auth.signOut();
-    const userRef = doc(firestore, 'users', userId);
-    const docSnapshot = await getDoc(userRef);
-    
-    if(docSnapshot.exists()) {
-      const data = docSnapshot.data();
-      // Si tiene la verificacion activada al cerrar sesion ponemos en false la verificacion completada
-      if ("twoFAEnabled" in data) {
-        await updateDoc(userRef, { twoFAVerified: false });
-        //localStorage.setItem('twoFACompleted', 'false');
+  const handleLogout = async () => {
+    const userId = auth.currentUser?.uid;
+    if (userId) {
+      await auth.signOut();
+      const userRef = doc(firestore, 'users', userId);
+      const docSnapshot = await getDoc(userRef);
+      if (docSnapshot.exists()) {
+        const data = docSnapshot.data();
+        if ("twoFAEnabled" in data) {
+          await updateDoc(userRef, { twoFAVerified: false });
+        }
       }
     }
-
-    
-  }
-};
-
+  };
 
   const renderContent = () => {
     if (showProfile) {
-        return <Perfil />;
+      return <Perfil />;
     }
     switch (selectedMenu) {
-        case '2':
-            return <Usuarios />;
-        case '3':
-            return <Perfil />;    
-        default:
-            return <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                Contenido principal del panel
-            </div>;
+      case '2':
+        return <Usuarios darkMode={darkMode} />;
+      case '3':
+        return <Perfil />;
+      case '4':
+        return <Proyectos setProyectoSeleccionado={setProyectoSeleccionado} />;
+      case '5':
+        return <Tareas proyectoSeleccionado={proyectoSeleccionado} />;
+      case '6':
+        return <Precios />  
+      default:
+        return <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+          Contenido principal del panel
+        </div>;
     }
-};
-
-
-
-
-
+  };
 
   return (
-    <Layout style={{ minHeight: '100vh' }}>
+    <Layout style={{ minHeight: '100vh', backgroundColor: darkMode ? '#121212' : '#f4f4f4' }}>
       <Sider
         breakpoint="lg"
         collapsedWidth="0"
+        style={{ backgroundColor: darkMode ? '#000' : 'inherit' }}
       >
         <div className="logo" />
-        <Menu theme="dark" mode="inline" selectedKeys={[selectedMenu]} onSelect={({ key }) => {
-          setSelectedMenu(key);
-          setShowProfile(false);
-        }}>
-          <Menu.Item key="1">Inicio</Menu.Item>
-          {userRole === 'admin' && <Menu.Item key="2">Usuarios</Menu.Item>}
-          <Menu.Item key="3">Perfil</Menu.Item>
+        <Menu 
+          style={menuStyle}
+          mode="inline"
+          selectedKeys={[selectedMenu]}
+          onSelect={({ key }) => {
+            setSelectedMenu(key);
+            setShowProfile(false);
+          }}
+        >
+          <Menu.Item style={selectedMenu === '1' ? selectedMenuItemStyle : menuItemStyle} key="1">Inicio</Menu.Item>
+          {userRole === 'admin' && <Menu.Item style={selectedMenu === '2' ? selectedMenuItemStyle : menuItemStyle} key="2">Usuarios</Menu.Item>}
+          <Menu.Item style={selectedMenu === '3' ? selectedMenuItemStyle : menuItemStyle} key="3">Perfil</Menu.Item>
+          <Menu.Item style={selectedMenu === '4' ? selectedMenuItemStyle : menuItemStyle} key="4">Proyectos</Menu.Item>
+          <Menu.Item style={selectedMenu === '5' ? selectedMenuItemStyle : menuItemStyle} key="5">Tareas</Menu.Item>
+          <Menu.Item style={selectedMenu === '6' ? selectedMenuItemStyle : menuItemStyle} key="6">Precios</Menu.Item>
         </Menu>
       </Sider>
       <Layout>
-        <Header className="site-layout-sub-header-background">
-        <Avatar src={userPhotoURL || userPhoto} style={{ marginRight: '10px' }} onClick={handleAvatarClick} />
-          <Button type="primary" onClick={handleLogout}>
-            Cerrar sesión
-          </Button>
+        <Header className="site-layout-sub-header-background" style={{ background: darkMode ? '#1E1E1E' : '#fff', color: darkMode ? '#fff' : '#333' }}>
+          <Avatar src={userPhotoURL} style={{ marginRight: '10px' }} onClick={handleAvatarClick} />
+          <Button icon={<LogoutOutlined />} onClick={handleLogout}>Cerrar sesión</Button>
+
         </Header>
-        <Content style={{ margin: '24px 16px 0' }}>
+        <Content style={{ margin: '0', background: darkMode ? '#121212' : '#fff', color: darkMode ? '#fff' : '#333' }}>
           {renderContent()}
         </Content>
-        <Footer style={{ textAlign: 'center' }}>Tu Empresa ©2023 Creado por Tu Nombre</Footer>
+        <Footer style={{ textAlign: 'center', background: darkMode ? '#333' : '#fff', color: darkMode ? '#fff' : '#333' }}>Tu Empresa ©2023 Creado por Tu Nombre</Footer>
       </Layout>
+      <FloatButton
+        style={{ bottom: 20, right: 20 }}
+        onClick={toggleDarkMode}
+        icon={<BulbOutlined />}
+      >
+        {darkMode ? 'Modo Claro' : 'Modo Oscuro'}
+      </FloatButton>
     </Layout>
   );
+  
 }
 
 export default Dashboard;
